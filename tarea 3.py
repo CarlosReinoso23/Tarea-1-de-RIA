1b6d8e2fbce814f05b7bca4d3b9730b5ed4092fa
CarlosReinoso23 <jose.reinoso@est.ucacue.edu.ec>
2024-05-04 00:37:33 -0500

    Add files via upload

---
+++ tarea 3.py
@@ -0,0 +1,6 @@
+"""Universidad Catolica de Cuenca
+Tarea 2
+Nombre: Carlos Reinoso"""
+n_1="Carlos Reinoso" #Variable que almacena un nombre
+letras_1=len(n_1) #Variable que almacena el numero de letras del nombre
+print("Su nombre tiene", letras_1, "letras")
