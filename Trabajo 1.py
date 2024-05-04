008ada8657c5c867add41240d6881893a005de65
CarlosReinoso23 <jose.reinoso@est.ucacue.edu.ec>
2024-05-04 00:23:22 -0500

    Add files via upload

---
+++ Trabajo 1.py
@@ -0,0 +1,15 @@
+"""Tarea
+Nombre= Carlos Reinoso"""
+n_1 =8 #variable n_1 y n_2 son tipo entero
+n_2 =8
+n_3 =24.1 #Variable n_3 y n_4 son tipo punto flotante
+n_4 =89.9
+suma = n_1+n_2 # Variable que almacena la suma resta
+resta= n_3-n_4 # Variable que almacena la resta
+multiplicacion = n_1*n_2 # Variable que almacena la multiplicacion
+division = n_3/n_4 # Variable que almacena la division
+print("La suma es:", suma) #print nos ayuda a mostrar el texto en pantalla siempre usando comillas
+print("La resta es:", resta)
+print("La multiplicacion es:", multiplicacion)
+print("La division es:", division)
+
