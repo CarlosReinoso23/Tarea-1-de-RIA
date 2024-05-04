ea8ce6d2b6df9614f3666d3c43bcce42db55899d
CarlosReinoso23 <jose.reinoso@est.ucacue.edu.ec>
2024-05-04 00:44:05 -0500

    Add files via upload

---
+++ tarea 4.py
@@ -0,0 +1,9 @@
+"""Universidad Catolica de Cuenca
+Tarea 3
+Nombre: Carlos Reinoso"""
+n_1=3 #Variable de tipo entero
+n_2=9.3 #Variable de tipo punto flotante
+cambio_de_variable_1=float(n_1) #Almacena el valor de n_1 y la transforma a flotante
+cambio_de_variable_2=int(n_1) #Almacena el valor de n_2 y la transforma a entero
+print("El  numero entero",n_1 ,"a flotante: ", cambio_de_variable_1)
+print("El numero flotante", n_2 , " a entero: ", cambio_de_variable_2)
